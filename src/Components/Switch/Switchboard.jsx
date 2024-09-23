794f3bc4e5afd6e8bec01fbfb51abfdb1a3e6f63
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2024-09-23 17:55:55 +0100

    Added more switches to the board
    Changes
    - Addition of more mp3 files
    - Addition of 5 more switches
    - testing of counter when switch is pressed
    - addition of image folder

--- src/Components/Switch/Switchboard.jsx
+++ src/Components/Switch/Switchboard.jsx
@@ -1,6 +1,7 @@
 
 import {useState} from 'react';
 import Switch from './switch';
+
 /* Audio imports */
 import light from '../../assets/audio/light-switch.mp3';
 import one from '../../assets/audio/Switch sound/Youre too slow.mp3';
@@ -8,19 +9,35 @@ import two from '../../assets/audio/Switch sound/Handsome Jack.mp3';
 import three from '../../assets/audio/Switch sound/IDubbbzTV.mp3';
 import four from '../../assets/audio/Switch sound/Minecraft cow sound effect.mp3';
 import five from '../../assets/audio/Switch sound/Roblox Death Sound - OOF.mp3';
+import six from '../../assets/audio/Switch sound/What the dog doin.mp3';
+import seven from '../../assets/audio/Switch sound/drip-goku-meme-song-original-dragon-ball-super-music-clash-of-gods-in-description.mp3';
+import eight from '../../assets/audio/Switch sound/crazy-realistic-knocking-sound-troll-twitch-streamers_small.mp3';
+import nine from '../../assets/audio/Switch sound/discord-call-sound.mp3';
+import nine1 from '../../assets/audio/Switch sound/discord-notification.mp3';
+import ten from '../../assets/audio/Switch sound/get-out-tuco.mp3';
 
 function App() {
   const [Start, setStart] = useState(true);
+  const [counter, setCounter] = useState(0);
+
   const [audio1] = useState(new Audio(one));
   const [audio2] = useState(new Audio(two));
   const [audio3] = useState(new Audio(three));
   const [audio4] = useState(new Audio(four));
   const [audio5] = useState(new Audio(five));
+  const [audio6] = useState(new Audio(six));
+  const [audio7] = useState(new Audio(seven));
+  const [audio8] = useState(new Audio(eight));
+  const [audio9] = useState(new Audio(nine));
+  const [audio91] = useState(new Audio(nine1));
+  const [audio10] = useState(new Audio(ten));
 
   const switchSound = () => {
     const audio = new Audio(light);
     audio.play();
   };
+
+  // Numbered Audios below
 
   const One = () => {
     setStart(!Start);
@@ -77,6 +94,71 @@ function App() {
     }
   };
 
+  const Six = () => {
+    setStart(!Start);
+
+    if (Start) {
+      audio6.play();
+    } else {
+      audio6.pause();
+      audio6.currentTime = 0;
+    }
+  };
+
+
+  const Seven = () => {
+    setStart(!Start);
+
+    if (Start) {
+      audio7.play();
+    } else {
+      audio7.pause();
+      audio7.currentTime = 0;
+    }
+  };
+
+
+  const Eight = () => {
+    setStart(!Start);
+
+    if (Start) {
+      audio8.play();
+    } else {
+      audio8.pause();
+      audio8.currentTime = 0;
+    }
+  };
+
+
+  const Nine = () => {
+    setStart(!Start);
+
+    if (Start && counter % 2 === 0 ) {
+      audio9.play();
+      setCounter((counter) => counter + 1);
+    
+    } else if (Start) {
+      audio91.play();
+      setCounter((counter) => counter + 1);
+    } else {
+      audio9.pause();
+      audio9.currentTime = 0;
+      audio91.pause();
+      audio91.currentTime = 0;
+    }
+  };
+
+  const Ten = () => {
+    setStart(!Start);
+
+    if (Start) {
+      audio10.play();
+    } else {
+      audio10.pause();
+      audio10.currentTime = 0;
+    }
+  };
+
 
   return (
     <>
@@ -85,6 +167,11 @@ function App() {
           <Switch onClick={() => {switchSound(); Three();}}/>
           <Switch onClick={() => {switchSound(); Four();}}/>
           <Switch onClick={() => {switchSound(); Five();}}/>
+          <Switch onClick={() => {switchSound(); Six();}}/>
+          <Switch onClick={() => {switchSound(); Seven();}}/>
+          <Switch onClick={() => {switchSound(); Eight();}}/>
+          <Switch onClick={() => {switchSound(); Nine();}}/>
+          <Switch onClick={() => {switchSound(); Ten();}}/>
     </>
   )
 }

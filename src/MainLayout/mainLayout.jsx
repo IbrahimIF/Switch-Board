809b8f63919051069067fe847c1b2f0d4736826f
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2025-04-10 23:44:57 +0100

    Fixed and Updated blur-overlay

--- src/MainLayout/mainLayout.jsx
+++ src/MainLayout/mainLayout.jsx
@@ -2,7 +2,6 @@ import { useState,} from "react";
 import { useInView } from "react-intersection-observer";
 
 import Hub from '../Components/Hub';
-import Fakeswitch from '../Components/Switch/Switchboard';
 import Contact from '../Components/Contact/contact';
 import "./MainLayout.css";
 import '../App.css';
@@ -10,34 +9,23 @@ import '../App.css';
 const ScrollBlurOverlay = () => {
   const [counter, setCounter] = useState(0);
   const { ref: contactRef, inView: isContactInView } = useInView({
-    threshold: 0.5, // Trigger even if only 10% is visible
-    rootMargin: '-50px 0px 0px 0px' // Adjust trigger point upward
+    threshold: 0.8, 
+    rootMargin: '-50px 0px -50px 0px'
   });
-
-  return (
-    <div className="main-layout">
-
-      <div>
-      <div className={`hub-overlay ${isContactInView ? "visible" : ""}`}>
-
-      </div>
-      <Hub counter={counter} setCounter={setCounter} />
-      </div>
-
-
-      <div className="contact-container">
-      <div ref={contactRef} className="contact-trigger"/>
-       <div className={`contact-overlay ${isContactInView ? "visible" : ""}`}>
-          <Contact />
-      </div>
-         {/* Contact overlay (fixed position) */}
-        <div className="switch_container" >
-          <Fakeswitch />
-        </div>
-      </div>
 
   
 
+  return (
+    <div className="main-layout">
+      <div className={`main-overlay ${isContactInView ? "blurred" : ""}`}>
+        <Hub counter={counter} setCounter={setCounter} />
+      </div>
+        <div className="contact-container">
+          <div ref={contactRef} className="contact-trigger"/>
+          <div className={`contact-overlay ${isContactInView ? "visible" : ""}`} >
+            <Contact />
+          </div>
+        </div>
     </div>
   );
 };
